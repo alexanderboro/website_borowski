10b24c7cc38ef63908b1c080e8aa540acf66513a
Alexander Borowski <borowski.ventures@gmail.com>
2022-09-27 15:21:59 +0200

    redone everything

---
+++ myscripts.js
@@ -0,0 +1,19 @@
+
+// Opening the Menu in mobile view
+
+function openMenu() {
+    var element = document.getElementById("id_menu_show");
+    element.classList.toggle('top-navigation-links-on')
+    /*var element2 = document.getElementsByClassName("top-navigation-links-off");
+    element2.classList.toggle('') */
+  }
+
+// Closing the Menu in mobile view
+function closeMenu() {
+    var element = document.getElementById("id_menu_show");
+    element.classList.toggle('top-navigation-links-on')
+   /* var element2 = document.getElementsByClassName("top-navigation-links-on");
+    element2.classList.toggle('')*/
+}
+
+
