03b8da9f319895cf1d3252a13eb0f08c0cf31afe
Alexander Borowski <borowski.ventures@gmail.com>
2022-10-07 10:00:30 +0100

    <commit_message>

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`👋 Started server on port ${PORT}`)
+})
+
+app.get('/', (request, response) => {
+    response.send('Welcome to my Blog!')
+  })
