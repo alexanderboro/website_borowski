043fa22e0a9b5654b1b913e48d26791b1a5a6a47
Alexander Borowski <102802301+alexanderboro@users.noreply.github.com>
2023-07-23 17:36:10 +0200

    Storing only article, index, about and contact clicks
    edited logPageView middleware to only track certain pages.
    
    I tracked all PageViews before.
    
    I also cleaned MongoDB for all entries in the PageView collection.

--- middleware/logPageView.js
+++ middleware/logPageView.js
@@ -1,22 +1,27 @@
-import PageView from '../models/page-view.js'; 
+import PageView from '../models/page-view.js';
+
+const trackedUrls = ['/articles', '/', '/index', '/about', '/contact'];
 
 const logPageView = async (req, res, next) => {
   const url = req.originalUrl;
-  
-  try {
-    let pageView = await PageView.findOne({ pageUrl: url });
 
-    if (pageView) {
-      pageView.views++;
-    } else {
-      pageView = new PageView({ pageUrl: url, views: 1 });
+  // Checking if the current URL should be tracked
+  if (trackedUrls.some((trackedUrl) => url.startsWith(trackedUrl))) {
+    try {
+      let pageView = await PageView.findOne({ pageUrl: url });
+
+      if (pageView) {
+        pageView.views++;
+      } else {
+        pageView = new PageView({ pageUrl: url, views: 1 });
+      }
+
+      pageView.save().catch(err => console.error(err));
+    } catch(err) {
+      console.error(err);
     }
-    
-    pageView.save().catch(err => console.error(err));
-  } catch(err) {
-    console.error(err);
   }
-  
+
   next();
 };
 
