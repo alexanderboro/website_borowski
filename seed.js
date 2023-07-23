422b1a11ae9f8f6835cf0afbbe5fc5c76158d200
Alexander Borowski <102802301+alexanderboro@users.noreply.github.com>
2023-07-23 17:40:00 +0200

    Checking if user already exists
    Received a database duplicate in my terminal constantly and I now check if my user already exists if not, I create a new one. It's a very basic setup but it's enough for me as the author.

--- seed.js
+++ seed.js
@@ -6,23 +6,27 @@ dotenv.config({ path: '.env' });
 function connectToMongoDB() {
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.edwk7gv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-  .then(() => {
-    console.log('💿 Database connected to Mongo Db Atlas');
-    // const user = new User({ username: 'alexboro', password: '746shs6as&sdasd+a' });
-    const user = new User({ username: '12', password: '123' });
+.then(async () => {
+  console.log('💿 Database connected to Mongo Db Atlas');
 
-    // const userExists = await User.findOne({ username: 'alexboro' });
-    // user.save()
-    //   .then(() => {
-    //     console.log('User created');
-    //     mongoose.connection.close();
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //     mongoose.connection.close();
-    //   });
-  })
-  .catch(error => console.error(error));
+  // Check if user already exists
+  const existingUser = await User.findOne({ username: 'boro' });
+
+  // If the user does not exist, create a new one
+  if (!existingUser) {
+    const user = new User({ username: 'boro', password: '123' });
+    user.save()
+      .then(() => {
+        console.log('User boro created');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  } else {
+    console.log('User boro already exists');
+  }
+})
+.catch(error => console.error(error));
 }
 
 export default connectToMongoDB;
