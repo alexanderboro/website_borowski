960deade2a1f93834268103961bd2cd8f4b82b01
Alexander Borowski <102802301+alexanderboro@users.noreply.github.com>
2023-07-26 11:04:46 +0200

    Implemented theme switcher, updated articles and footer
    Implemented a theme switcher that dynamically changes the background color. And I refined the footer of most of my pages.
    And I added content to some static html and created a new article instance in my MongoDB

--- public/myscripts.js
+++ public/myscripts.js
@@ -16,4 +16,25 @@ function closeMenu() {
     element2.classList.toggle('')*/
 }
 
+// Switching my background color 'dark-mode' on all pages for users
+const themeSwitcher = document.querySelector('#theme-switcher');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode');
+  themeSwitcher.textContent = 'go classic';
+} else {
+  themeSwitcher.textContent = 'go wavvy';
+}
+
+themeSwitcher.addEventListener('click', function() {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light');
+    themeSwitcher.textContent = 'go wavvy';
+  } else {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+    themeSwitcher.textContent = 'go classic';
+  }
+});
 
